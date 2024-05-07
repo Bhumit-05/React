c0620543f47650f1fc4bef182091f921c2942e83
Bhumit-05 <bhumit05janghel@gmail.com>
2024-05-07 18:39:35 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading=React.createElement("div", {id: parent}, 
+    React.createElement("div", {id: child}, [
+        React.createElement("h1", {}, "H1 tag used"), 
+        React.createElement("h2", {}, "H2 tag used")
+    ])
+);
+
+console.log(heading);
+
+const r=ReactDOM.createRoot(document.getElementById("root"));
+
+r.render(heading);
