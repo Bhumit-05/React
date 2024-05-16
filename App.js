88774ac0f03e8df03f07f9d46404eacb223d6348
Bhumit-05 <bhumit05janghel@gmail.com>
2024-05-16 06:20:30 +0530

    App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-const heading=React.createElement("div", {id: parent}, 
-    React.createElement("div", {id: child}, [
+const heading=React.createElement("div", {id: "parent"}, 
+    React.createElement("div", {id: "child"}, [
         React.createElement("h1", {}, "H1 tag used"), 
         React.createElement("h2", {}, "H2 tag used")
     ])
